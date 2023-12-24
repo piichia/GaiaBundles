967fe18f14281b7ccfe0d89f247964e422d95c22
piichia <shika.chn@gmail.com>
2023-12-23 22:57:38 -0600

    1

---
+++ umamidrop.js
@@ -0,0 +1,128 @@
+//first is common, second is rare
+let umamiList = [
+    {
+        name: 'Umami Drop: Cryptic Radiologist ',
+        itemData: '10527296',
+        contains: ['10526554','10527464']
+    },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+    // {
+    //     name: '',
+    //     itemData: '',
+    //     contains: ['','']
+    // },
+]
