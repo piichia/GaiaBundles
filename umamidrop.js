57e43d202b592c4f5c34512d5a859fc4e1608b80
piichia <shika.chn@gmail.com>
2023-12-23 23:18:13 -0600

    1

--- umamidrop.js
+++ umamidrop.js
@@ -15,31 +15,31 @@ let umamiList = [
         itemData: '10519376',
         contains: ['10519044','10519540']
     },
-    // {
-    //     name: '',
-    //     itemData: '',
-    //     contains: ['','']
-    // },
-    // {
-    //     name: '',
-    //     itemData: '',
-    //     contains: ['','']
-    // },
-    // {
-    //     name: '',
-    //     itemData: '',
-    //     contains: ['','']
-    // },
-    // {
-    //     name: '',
-    //     itemData: '',
-    //     contains: ['','']
-    // },
-    // {
-    //     name: '',
-    //     itemData: '',
-    //     contains: ['','']
-    // },
+    {
+        name: 'Umami Drop: Cardinal Friendship',
+        itemData: '10654380',
+        contains: ['10651782','10654026']
+    },
+    {
+        name: 'Umami Drop: Sióg Na Féile ',
+        itemData: '10787400',
+        contains: ['10786068','10786827']
+    },
+    {
+        name: 'Umami Drop: 1UP Superstar ',
+        itemData: '10507826',
+        contains: ['10505336','10507038']
+    },
+    {
+        name: 'Umami Drop: Cristallis ',
+        itemData: '10579947',
+        contains: ['10579794','10579950']
+    },
+    {
+        name: 'Umami Drop: Close Memory ',
+        itemData: '10588701',
+        contains: ['10588185','10588647']
+    },
     // {
     //     name: '',
     //     itemData: '',
