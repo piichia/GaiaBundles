da6b06a17f80fd90fb65a1a3ebc41587fa1e8c7b
piichia <shika.chn@gmail.com>
2023-12-23 23:10:34 -0600

    1

--- umamidrop.js
+++ umamidrop.js
@@ -5,16 +5,16 @@ let umamiList = [
         itemData: '10527296',
         contains: ['10526554','10527464']
     },
-    // {
-    //     name: '',
-    //     itemData: '',
-    //     contains: ['','']
-    // },
-    // {
-    //     name: '',
-    //     itemData: '',
-    //     contains: ['','']
-    // },
+    {
+        name: 'Umami Drop: Wolfsbane ',
+        itemData: '10519378',
+        contains: ['10518826','10519016']
+    },
+    {
+        name: 'Umami Drop: Lightbringer ',
+        itemData: '10519376',
+        contains: ['10519044','10519540']
+    },
     // {
     //     name: '',
     //     itemData: '',
