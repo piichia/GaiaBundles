87f0a58d7ccd93594c5041c23ecd1094c172638b
HannahCBr <hannah.brumfield11@gmail.com>
2025-01-24 15:44:11 -0600

    1

---
+++ missingBundles.js
@@ -0,0 +1,130 @@
+let missingList = [
+    {
+        name: "Crystalline Dreams Embers Bundle ",
+        itemData: "11191107"
+    },
+    {
+        name: "Flutter Fantasy Konpeito Bundle ",
+        itemData: "11160396"
+    },
+    {
+        name: "Zoo? Zoo? Zoomin'! Envious Bundle ",
+        itemData: "11152368"
+    },
+    {
+        name: "Booked & Busy II Withered Bundle ",
+        itemData: "11146239"
+    },
+    {
+        name: "Cottage Fever Iridescent Bundle ",
+        itemData: "11146122"
+    },
+    {
+        name: "Noble Intentions Wondrous Bundle ",
+        itemData: "11140536"
+    },
+    {
+        name: "Noble Intentions Blazing Bundle ",
+        itemData: "11140539"
+    },
+    {
+        name: "City of Sin Eternal Bundle ",
+        itemData: "11137374"
+    },
+    {
+        name: "City of Sin Alexandrite Bundle ",
+        itemData: "11137377"
+    },
+    {
+        name: "Puff Patrol Berry Bundle ",
+        itemData: "11137929"
+    },
+    {
+        name: "Spring's Splendors II Brilliance Bundle ",
+        itemData: "11134842"
+    },
+    {
+        name: "Spring's Splendors II Idyllic Bundle ",
+        itemData: "11134845"
+    },
+    {
+        name: "Fairly Fairy Seafaring Bundle ",
+        itemData: "11130960"
+    },
+    {
+        name: "Lunar Revelry Iridescent Bundle ",
+        itemData: "11104674"
+    },
+    {
+        name: "Cheerful Doll Trunk Tip-Top Bundle ",
+        itemData: "11087766"
+    },
+    {
+        name: "Very Superstitious Alexandrite Bundle ",
+        itemData: "11066406"
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+    {
+        name: "",
+        itemData: ""
+    },
+]
