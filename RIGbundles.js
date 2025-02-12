11b07e2d6b3f58bba0a2812d2a7983c4f657dc84
HannahCBr <hannah.brumfield11@gmail.com>
2025-02-12 15:10:03 -0600

    1

---
+++ RIGbundles.js
@@ -0,0 +1,632 @@
+let RIGbundles = [
+    {
+        name: "Extra Extra Abyssal Bundle ",
+        bundleID: "11203833",
+        itemIDs: ["11202480"],
+        tenPack: "11203821",
+        single: "11203815"
+    },
+    {
+        name: "Extra Extra Kaguya Bundle ",
+        bundleID: "11203830",
+        itemIDs: ["11204526"],
+        tenPack: "11203821",
+        single: "11203815"
+    },
+    {
+        name: "Perfect 'Fit Jellybean Bundle ",
+        bundleID: "11200197",
+        itemIDs: ["11198829","11198793"],
+        tenPack: "11200185",
+        single: "11200179"
+    },
+    {
+        name: "Perfect 'Fit Edgy Bundle ",
+        bundleID: "11200194",
+        itemIDs: ["11198739"],
+        tenPack: "11200185",
+        single: "11200179"
+    },
+    {
+        name: "Time Honored Abyssal Bundle ",
+        bundleID: "11196897",
+        itemIDs: ["11197155"],
+        tenPack: "11196885",
+        single: "11196879"
+    },
+    {
+        name: "Time Honored Beloved Bundle ",
+        bundleID: "11196894",
+        itemIDs: ["11198493","11198553"],
+        tenPack: "11196885",
+        single: "11196879"
+    },
+    {
+        name: "A Little Flourish Aocha Bundle ",
+        bundleID: "11195790",
+        itemIDs: ["11196516"],
+        tenPack: "11195778",
+        single: "11195772"
+    },
+    {
+        name: "A Little Flourish Pure Bundle ",
+        bundleID: "11195787",
+        itemIDs: ["11196051"],
+        tenPack: "11195778",
+        single: "11195772"
+    },
+    {
+        name: "Scenes of Beauty II Jellybean Bundle ",
+        bundleID: "11194866",
+        itemIDs: ["11193915"],
+        tenPack: "11194854",
+        single: "11194848"
+    },
+    {
+        name: "Scenes of Beauty II Coarse Bundle ",
+        bundleID: "11194863",
+        itemIDs: ["11194572"],
+        tenPack: "11194854",
+        single: "11194848"
+    },
+    {
+        name: "Crystalline Dreams Candied Bundle ",
+        bundleID: "11191104",
+        itemIDs: ["11191350"],
+        tenPack: "11191095",
+        single: "11191053"
+    },
+    {
+        name: "Crystalline Dreams Embers Bundle ",
+        bundleID: "11191107",
+        itemIDs: ["11192685"],
+        tenPack: "11191095",
+        single: "11191053"
+    },
+    {
+        name: "Warmest Wishes Abyssal Bundle ",
+        bundleID: "11186781",
+        itemIDs: ["11189847","11189811"],
+        tenPack: "11186769",
+        single: "11186763"
+    },
+    {
+        name: "Warmest Wishes Classic Bundle ",
+        bundleID: "11186778",
+        itemIDs: ["11189745"],
+        tenPack: "11186769",
+        single: "11186763"
+    },
+    {
+        name: "Among the Stars Dancer Bundle ",
+        bundleID: "11186577",
+        itemIDs: ["11187096"],
+        tenPack: "11186565",
+        single: "11186559"
+    },
+    {
+        name: "Among the Stars Creacher Bundle ",
+        bundleID: "11186574",
+        itemIDs: ["11188896"],
+        tenPack: "11186565",
+        single: "11186559"
+    },
+    {
+        name: "#Mood Pure Bundle ",
+        bundleID: "11185479",
+        itemIDs: ["11186277"],
+        tenPack: "11185467",
+        single: "11185461"
+    },
+    {
+        name: "#Mood Zen Bundle ",
+        bundleID: "11185476",
+        itemIDs: ["11186469"],
+        tenPack: "11185467",
+        single: "11185461"
+    },
+    {
+        name: "Scenes of Beauty Autumnal Bundle ",
+        bundleID: "11180778",
+        itemIDs: ["11182590"],
+        tenPack: "11180766",
+        single: "11180760"
+    },
+    {
+        name: "Scenes of Beauty Sentimental Bundle ",
+        bundleID: "11180775",
+        itemIDs: ["11180943"],
+        tenPack: "11180766",
+        single: "11180760"
+    },
+    {
+        name: "All Over Creation Art Bundle ",
+        bundleID: "11179584",
+        itemIDs: ["11181237","11181291"],
+        tenPack: "11179572",
+        single: "11179566"
+    },
+    {
+        name: "All Over Creation Jelly Bundle ",
+        bundleID: "11179581",
+        itemIDs: ["11181177"],
+        tenPack: "11179572",
+        single: "11179566"
+    },
+    {
+        name: "The Unknown Abyssal Bundle ",
+        bundleID: "11178870",
+        itemIDs: ["11179014"],
+        tenPack: "11178858",
+        single: "11178852"
+    },
+    {
+        name: "The Unknown Sadistic Bundle ",
+        bundleID: "11178867",
+        itemIDs: ["11178444"],
+        tenPack: "11178858",
+        single: "11178852"
+    },
+    {
+        name: "Tea & Crumpets Coarse Bundle ",
+        bundleID: "11178120",
+        itemIDs: ["11177616"],
+        tenPack: "11178108",
+        single: "11178102"
+    },
+    {
+        name: "Tea & Crumpets Aocha Bundle ",
+        bundleID: "11178117",
+        itemIDs: ["11177481"],
+        tenPack: "11178108",
+        single: "11178102"
+    },
+    {
+        name: "Be Witched Forage Bundle ",
+        bundleID: "11175492",
+        itemIDs: ["11174751"],
+        tenPack: "11175480",
+        single: "11175474"
+    },
+    {
+        name: "Be Witched Be Loved Bundle ",
+        bundleID: "11175489",
+        itemIDs: ["11175882"],
+        tenPack: "11175480",
+        single: "11175474"
+    },
+    {
+        name: "Hair-raising Sanguine Bundle ",
+        bundleID: "11172150",
+        itemIDs: ["11170767","11171676","11172153"],
+        tenPack: "11172138",
+        single: "11172132"
+    },
+    {
+        name: "Hair-raising Gore-geous Bundle ",
+        bundleID: "11172147",
+        itemIDs: ["11170737","11171766","11171889"],
+        tenPack: "11172138",
+        single: "11172132"
+    },
+    {
+        name: "Mysterious & Spooky Wondrous Bundle ",
+        bundleID: "11172120",
+        itemIDs: ["11172279"],
+        tenPack: "11172108",
+        single: "11172102"
+    },
+    {
+        name: "Mysterious & Spooky Alluring Bundle ",
+        bundleID: "11172117",
+        itemIDs: ["11172495"],
+        tenPack: "11172108",
+        single: "11172102"
+    },
+    {
+        name: "Aura Points Holo Bundle ",
+        bundleID: "11167035",
+        itemIDs: ["11166537"],
+        tenPack: "11167023",
+        single: "11167017"
+    },
+    {
+        name: "Aura Points Harlequin Bundle ",
+        bundleID: "11167032",
+        itemIDs: ["11168154"],
+        tenPack: "11167023",
+        single: "11167017"
+    },
+    {
+        name: "Estival Encore Rebel Bundle ",
+        bundleID: "11166018",
+        itemIDs: ["11166207","11165649"],
+        tenPack: "11166003",
+        single: "11165997"
+    },
+    {
+        name: "Estival Encore Royal Bundle ",
+        bundleID: "11166015",
+        itemIDs: ["11166141","11165559"],
+        tenPack: "11166003",
+        single: "11165997"
+    },
+    {
+        name: "Estival Encore Revel Bundle ",
+        bundleID: "11166012",
+        itemIDs: ["11165457","11165589"],
+        tenPack: "11166003",
+        single: "11165997"
+    },
+    {
+        name: "All That Glitters Classic Bundle ",
+        bundleID: "11164194",
+        itemIDs: ["11167803"],
+        tenPack: "11164182",
+        single: "11164176"
+    },
+    {
+        name: "All That Glitters Sweet Bundle ",
+        bundleID: "11164191",
+        itemIDs: ["11168352"],
+        tenPack: "11164182",
+        single: "11164176"
+    },
+    {
+        name: "A Little Stitious Coarse Bundle ",
+        bundleID: "11163273",
+        itemIDs: ["11161533"],
+        tenPack: "11160795",
+        single: "11160789"
+    },
+    {
+        name: "A Little Stitious Pure Bundle ",
+        bundleID: "11163270",
+        itemIDs: ["11163132"],
+        tenPack: "11160795",
+        single: "11160789"
+    },
+    {
+        name: "Decora is More-a Sincere Bundle ",
+        bundleID: "11162157",
+        itemIDs: ["11162079"],
+        tenPack: "11162145",
+        single: "11162139"
+    },
+    {
+        name: "Decora is More-a Iridescent Bundle ",
+        bundleID: "11162154",
+        itemIDs: ["11161302"],
+        tenPack: "11162145",
+        single: "11162139"
+    },
+    {
+        name: "Flutter Fantasy Aocha Bundle ",
+        bundleID: "11160399",
+        itemIDs: ["11157654"],
+        tenPack: "11160387",
+        single: "11160381"
+    },
+    {
+        name: "Flutter Fantasy Konpeito Bundle ",
+        bundleID: "11160396",
+        itemIDs: ["11160489"],
+        tenPack: "11160387",
+        single: "11160381"
+    },
+    {
+        name: "Whimsy & Wonder Coarse Bundle ",
+        bundleID: "11156070",
+        itemIDs: ["11157711"],
+        tenPack: "11156058",
+        single: "11156052"
+    },
+    {
+        name: "Whimsy & Wonder Pure Bundle ",
+        bundleID: "11156067",
+        itemIDs: ["11157804"],
+        tenPack: "11156058",
+        single: "11156052"
+    },
+    {
+        name: "Steel and Honor Wondrous Bundle ",
+        bundleID: "11155497",
+        itemIDs: ["11153418"],
+        tenPack: "11155485",
+        single: "11155479"
+    },
+    {
+        name: "Steel and Honor Iridescent Bundle ",
+        bundleID: "11155494",
+        itemIDs: ["11153472"],
+        tenPack: "11155485",
+        single: "11155479"
+    },
+    {
+        name: "Pairfection Boned Bundle ",
+        bundleID: "11153760",
+        itemIDs: ["11154558","11154627"],
+        tenPack: "11153748",
+        single: "11153742"
+    },
+    {
+        name: "Pairfection Aocha Bundle ",
+        bundleID: "11153757",
+        itemIDs: ["11154180","11154234"],
+        tenPack: "11153748",
+        single: "11153742"
+    },
+    {
+        name: "Zoo? Zoo? Zoomin'! Envious Bundle ",
+        bundleID: "11152368",
+        itemIDs: ["11152767","11152710"],
+        tenPack: "11152356",
+        single: "11152350"
+    },
+    {
+        name: "Zoo? Zoo? Zoomin'! Cosmic Bundle ",
+        bundleID: "11152365",
+        itemIDs: ["11151012"],
+        tenPack: "11152356",
+        single: "11152350"
+    },
+    {
+        name: "Shining Through Iridescent Bundle ",
+        bundleID: "11148045",
+        itemIDs: ["11150637"],
+        tenPack: "11148033",
+        single: "11148027"
+    },
+    {
+        name: "Shining Through Soft Bundle ",
+        bundleID: "11148042",
+        itemIDs: ["11150502"],
+        tenPack: "11148033",
+        single: "11148027"
+    },
+    {
+        name: "Snipped Off Enlightened Bundle ",
+        bundleID: "11146695",
+        itemIDs: ["11148810","11149755"],
+        tenPack: "11146680",
+        single: "11146674"
+    },
+    {
+        name: "Snipped Off Luscious Bundle ",
+        bundleID: "11146689",
+        itemIDs: ["11147223","11147277"],
+        tenPack: "11146680",
+        single: "11146674"
+    },
+    {
+        name: "Booked & Busy II Heavenly Bundle ",
+        bundleID: "11146236",
+        itemIDs: ["11146698"],
+        tenPack: "11146227",
+        single: "11146221"
+    },
+    {
+        name: "Booked & Busy II Withered Bundle ",
+        bundleID: "11146239",
+        itemIDs: ["11146461"],
+        tenPack: "11146227",
+        single: "11146221"
+    },
+    {
+        name: "Cottage Fever Sincere Bundle ",
+        bundleID: "11146119",
+        itemIDs: ["11146158"],
+        tenPack: "11146110",
+        single: "11146104"
+    },
+    {
+        name: "Cottage Fever Iridescent Bundle ",
+        bundleID: "11146122",
+        itemIDs: ["11144871"],
+        tenPack: "11146110",
+        single: "11146104"
+    },
+    {
+        name: "Captivating Curios Lavish Bundle ",
+        bundleID: "11143692",
+        itemIDs: ["11143581","11143269","11143404"],
+        tenPack: "11143680",
+        single: "11143674"
+    },
+    {
+        name: "Captivating Curios Victory Bundle ",
+        bundleID: "11143689",
+        itemIDs: ["11143209","11143764","11143548"],
+        tenPack: "11143680",
+        single: "11143674"
+    },
+    {
+        name: "Puff Patrol Candy Bundle ",
+        bundleID: "11137926",
+        itemIDs: ["11139930"],
+        tenPack: "11137917",
+        single: "11137911"
+    },
+    {
+        name: "Puff Patrol Berry Bundle ",
+        bundleID: "11137929",
+        itemIDs: ["11140179"],
+        tenPack: "11137917",
+        single: "11137911"
+    },
+    {
+        name: "Eastern Elegance Sincere Bundle ",
+        bundleID: "11130036",
+        itemIDs: ["11131542","11131602"],
+        tenPack: "11130024",
+        single: "11130018"
+    },
+    {
+        name: "Eastern Elegance Sprinkles Bundle ",
+        bundleID: "11130033",
+        itemIDs: ["11132475"],
+        tenPack: "11130024",
+        single: "11130018"
+    },
+    {
+        name: "Floral Fantasy Sincere Bundle ",
+        bundleID: "11128662",
+        itemIDs: ["11126625","11126679"],
+        tenPack: "11128650",
+        single: "11128644"
+    },
+    {
+        name: "Floral Fantasy Wondrous Bundle ",
+        bundleID: "11128659",
+        itemIDs: ["11127246"],
+        tenPack: "11128650",
+        single: "11128644"
+    },
+    {
+        name: "Spring's Splendors Astonishing Bundle ",
+        bundleID: "11125422",
+        itemIDs: ["11126562","11123658"],
+        tenPack: "11125410",
+        single: "11125404"
+    },
+    {
+        name: "Spring's Splendors Marvelous Bundle ",
+        bundleID: "11125419",
+        itemIDs: ["11123598","11126274"],
+        tenPack: "11125410",
+        single: "11125404"
+    },
+    {
+        name: "Spring's Splendors II Brilliance Bundle ",
+        bundleID: "11134842",
+        itemIDs: ["11135085","11134545"],
+        tenPack: "11134833",
+        single: "11134827"
+    },
+    {
+        name: "Spring's Splendors II Idyllic Bundle ",
+        bundleID: "11134845",
+        itemIDs: ["11134425","11135055"],
+        tenPack: "11134833",
+        single: "11134827"
+    },
+    {
+        name: "Fairly Fairy Seafaring Bundle ",
+        bundleID: "11130960",
+        itemIDs: ["11131359","11131398"],
+        tenPack: "11130948",
+        single: "11130942"
+    },
+    {
+        name: "Fairly Fairy Iridescent Bundle ",
+        bundleID: "11130957",
+        itemIDs: ["11131320"],
+        tenPack: "11130948",
+        single: "11130942"
+    },
+    {
+        name: "Myths & Magic Flamboyant Bundle ",
+        bundleID: "11125398",
+        itemIDs: ["11124003"],
+        tenPack: "11125386",
+        single: "11125380"
+    },
+    {
+        name: "Myths & Magic Tearful Bundle ",
+        bundleID: "11125395",
+        itemIDs: ["11123802"],
+        tenPack: "11125386",
+        single: "11125380"
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+    {
+        name: "",
+        bundleID: "",
+        itemIDs: [""],
+        tenPack: "",
+        single: ""
+    },
+]
